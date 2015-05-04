9734251e38e2c49f88d28c6952e129ff873c8440
Paul Cochrane <paul@liekut.de>
2015-05-04 18:30:01 +0200

    Test build and install time of rakudobrew

--- build-rakudo.sh
+++ build-rakudo.sh
@@ -1,8 +1,11 @@
 #/bin/bash
 
 date
-time git clone https://github.com/rakudo/rakudo.git
-cd rakudo
-time sudo perl Configure.pl --backends=moar --gen-nqp --gen-moar --prefix=/usr
-time sudo make install
+#time git clone https://github.com/rakudo/rakudo.git
+#cd rakudo
+#time sudo perl Configure.pl --backends=moar --gen-nqp --gen-moar --prefix=/usr
+#time sudo make install
+time git clone https://github.com/tadzik/rakudobrew ~/.rakudobrew
+time rakudobrew build moar
+time perl6 -e '"hello world".say'
 date
